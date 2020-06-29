7765126051dbcc7436bfbb2eb76e17449b830295
Steffen <steffen@indigo-nms.eu>
2020-06-30 01:02:49 +0200

    Reformattings

--- satellite/bot.py
+++ satellite/bot.py
@@ -11,17 +11,15 @@ cloudconvert.configure(api_key=CLOUD_CONVERT_API_KEY)
 
 
 def export_file(task_id, chat_id):
-    # get cloudconvert task
-    export_task = cloudconvert.Task.find(id=task_id)
-    # get file in task
-    file = export_task.get('result').get('files')[0]
     with tempfile.NamedTemporaryFile() as f:
+        # get cloudconvert task
+        export_task = cloudconvert.Task.find(id=task_id)
+        # get file in task
+        file = export_task.get('result').get('files')[0]
         # download file from cloudconvert
         cloudconvert.download(filename=f.name, url=file['url'])
         # upload again to Telegram
-        upload(chat_id, f.name,
-               display_name=file['filename'],
-               progress_callback=print)
+        upload(chat_id, f.name, display_name=file['filename'])
 
 
 def import_file(task_id, chat_id, message_id):
@@ -31,5 +29,4 @@ def import_file(task_id, chat_id, message_id):
         # get cloudconvert task
         upload_task = cloudconvert.Task.find(id=task_id)
         # upload again to cloudconvert
-        cloudconvert.Task.upload(
-            file_name=f.name, task=upload_task)
+        cloudconvert.Task.upload(file_name=f.name, task=upload_task)
