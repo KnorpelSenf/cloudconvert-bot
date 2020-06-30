00c15c56e2ce29ba2570f4cd476b3a20f753fe2f
Steffen <steffen@indigo-nms.eu>
2020-06-30 15:23:17 +0200

    Improve error handling

--- satellite/main.py
+++ satellite/main.py
@@ -17,12 +17,23 @@ app = Flask(__name__)
 executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
 
 
+def missing_field_error(e):
+    return ({
+        'error': True,
+        'message': 'Request is missing a field',
+        'field': e.args[0],
+    }, 400)
+
+
 @app.route('/' + CONTEXT_PATH + '/export', methods=['POST'])
 def exp():
     data = request.get_json()
-    api_key = data['api_key']
-    task_id = data['task_id']
-    chat_id = data['chat_id']
+    try:
+        api_key = data['api_key']
+        task_id = data['task_id']
+        chat_id = data['chat_id']
+    except KeyError as e:
+        return missing_field_error(e)
     executor.submit(export_file, api_key, task_id, chat_id)
     return ('', 202)
 
@@ -30,9 +41,12 @@ def exp():
 @app.route('/' + CONTEXT_PATH + '/import', methods=['POST'])
 def imp():
     data = request.get_json()
-    api_key = data['api_key']
-    task_id = data['task_id']
-    chat_id = data['chat_id']
-    message_id = data['message_id']
+    try:
+        api_key = data['api_key']
+        task_id = data['task_id']
+        chat_id = data['chat_id']
+        message_id = data['message_id']
+    except KeyError as e:
+        return missing_field_error(e)
     executor.submit(import_file, api_key, task_id, chat_id, message_id)
     return ('', 202)
